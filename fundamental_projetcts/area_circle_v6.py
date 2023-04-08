2c913bfdb75ef33b4cd0e733f1bba93498066e12
Danilo Gomes de Moura/home/demo/.virtualenvs/openaigptforpython/bin/python <danilogmoura@gmail.com>
2023-04-07 23:41:31 -0300

    Fundamentals 07 - Introduction to modules

---
+++ fundamental_projetcts/area_circle_v6.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from math import pi
+
+radius = input("Enter radius of circle: ")
+print("Area of circle is: ", pi * float(radius) ** 2)
+
+print("Module name is: ", __name__)
+print("Package name is: ", __package__)
---
+++ fundamental_projetcts/area_circle_v7.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from math import pi
+
+if __name__ == "__main__":
+    radius = input("Enter radius of circle: ")
+    print("Area of circle is: ", pi * float(radius) ** 2)
