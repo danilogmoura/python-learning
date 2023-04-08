3c7d4229f0d0c7ff1d5513ec4c24b1567f8bad5e
Danilo Gomes de Moura/home/demo/.virtualenvs/openaigptforpython/bin/python <danilogmoura@gmail.com>
2023-04-08 00:16:08 -0300

    Fundamentals 10 - Improving the hel function

---
+++ fundamental_projetcts/area_circle_v12.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from math import pi
+import sys
+
+
+def help():
+    print("Is necessary to inform the radius of circle.")
+    print("Usage: {} <radius>".format(sys.argv[0].split("/")[-1]))
+
+
+def circle(radius):
+    return pi * float(radius) ** 2
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        help()
+    else:
+        radius = sys.argv[1]
+        area = circle(radius)
+        print("Area of circle with radius", radius, "is", area)
