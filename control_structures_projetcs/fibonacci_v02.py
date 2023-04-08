ce8e2be02015ef26c9ce7c7236fb0af1de4c8f16
Danilo Gomes de Moura/home/demo/.virtualenvs/openaigptforpython/bin/python <danilogmoura@gmail.com>
2023-04-08 02:40:07 -0300

    Control structures 01 - Using packing to change variables

---
+++ control_structures_projetcs/fibonacci_v02.py
@@ -0,0 +1,14 @@
+def fibonacci(limit=100):
+    penultimate = 0
+    last = 1
+    print(f"{penultimate}, {last}", end=", ")
+
+    while last < limit:
+        next = penultimate + last
+        print(next, end=", ")
+        penultimate = last
+        last = next
+
+
+if __name__ == "__main__":
+    fibonacci(10000)
